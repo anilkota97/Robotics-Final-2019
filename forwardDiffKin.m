24c2176fdb4e8e1b485b5ab1d0966301f7204d3e
ALongStringOfNumbers <sjensen793@gmail.com>
2019-11-30 12:00:23 -0700

    Missing file

---
+++ forwardDiffKin.m
@@ -0,0 +1,60 @@
+function [outputArg1,outputArg2] = forwardDiffKin(a, d, alpha, theta, jointTypes, jointVel)
+%UNTITLED3 Compute robot forward kinematics
+%   Detailed explanation goes here
+
+%Generate the set of transformation matricies for the entire robot
+[T, symTmat] = genTransforms(a,d,alpha,theta);
+
+%Generate the Jacobian matrix from the transforms
+[J, symJ] = GenerateJacobian(T,jointTypes, symTmat);
+
+%Make the matrix square
+temp = size(J);
+numCol = temp(2);
+
+colTracker = zeros(1,numCol);
+rowtracker = zeros(1,6);
+
+%Strip the extra rows so the jacobian can be performed
+if(numCol ~= 6)
+    for i = 1:6
+        for j = 1:numCol
+            if(J(i,j) == 0)
+                colTracker(j) = 1;
+            end
+        end
+        if(sum(colTracker) == numCol)
+            rowtracker(i) = 1;
+        end
+        
+        colTracker = zeros(1,numCol);
+    end
+    
+    temp = 1;
+    for i = 1:length(rowtracker)
+        if(rowtracker(temp) == 1)
+            J(temp,:) = [];
+            symJ(temp,:) = [];
+            rowtracker(temp) = [];
+           
+        else
+            temp = temp+1;
+        end
+        
+    end
+end
+
+
+
+%Find the singularities
+%first find the determinant of the Jacobian matrix
+tempJ = det(J);
+tempsym = det(symJ);
+
+%Find the end effector velocity
+%calculates the end effector linear and rotational velocity
+Ve = J*jointVel;
+
+
+end
+
