03ba0ddec0755ab564f339bafae21d7582e3a1a5
ALongStringOfNumbers <sjensen793@gmail.com>
2019-11-30 11:59:41 -0700

    Work on the differential Kinematics'

--- GenerateJacobian.m
+++ GenerateJacobian.m
@@ -1,4 +1,4 @@
-function [J] = GenerateJacobian(Tmat,joints)
+function [J, symJ] = GenerateJacobian(Tmat,joints, symTmat)
 %UNTITLED4 Generate the Jacobian matrix for a general robotic arm
 %   Tmat is a 4x4xn transformation matrix, with n being the number of
 %   joints, and each layer of Tmat being the transformation matrix from 0
@@ -8,35 +8,46 @@ function [J] = GenerateJacobian(Tmat,joints)
 %Create the Jacobian Matrix
 J = zeros(6,length(joints));
 
+
 %Find the end effector pose
 Pe = Tmat(1:3,4,end);
+Pesym = symTmat(1:3,4,end);
 
 
 for i = 1:length(joints)
     %Check if the joint is prismatic
-    if(strcmpi(joints(i),'p'))
+    if(joints(i) == 'P' || joints(i) == 'p')
         if(i == 1)
             Zcomp = [0;0;1];
+            Zcompsym = [0;0;1];
         else
             Zcomp = Tmat(1:3,3,i-1);
+            Zcompsym = symTmat(1:3,3,i-1);
         end
         %Fill in the Jacobian
         J(:,i) = [Zcomp;0;0;0];
+        symJ(:,i) = [Zcompsym;0;0;0];
     %Revolute Joints
-    elseif(strcmpi(joints(i),'r'))
+    elseif(joints(i) == 'R' || joints(i) == 'r')
         
         %Find the P and the Z components
         if(i == 1)
             P = [0;0;0];
-            Zcomp = [0;0;1];            
+            Zcomp = [0;0;1]; 
+            Psym = [0;0;0];
+            Zcompsym = [0;0;1];
         else
             P = Tmat(1:3,4,i-1);
-            Zcomp = Tmat(1:3,3,i-1);            
+            Zcomp = Tmat(1:3,3,i-1);  
+            Psym = symTmat(1:3,4,i-1);
+            Zcompsym = symTmat(1:3,3,i-1);
         end
         %Find the Jacobian component
         Jcomp = cross(Zcomp,(Pe - P));
+        Jcompsym = cross(Zcompsym,(Pesym - Psym));
         %Create the Jacobian entry
         J(:,i) = [Jcomp;Zcomp];
+        symJ(:,i) = [Jcompsym;Zcompsym];
     end
 end
         
--- genTransforms.m
+++ genTransforms.m
@@ -1,4 +1,4 @@
-function [T] = genTransforms(a,d,alpha,theta)
+function [T,symT] = genTransforms(a,d,alpha,theta)
 %UNTITLED6 Generate Transformation matrices from DH parameters
 %   Detailed explanation goes here
 
@@ -12,18 +12,42 @@ for i = 1:length(a)
         0 sind(alpha(i)), cosd(alpha(i)), d(i); 0 0 0 1];
 end
 
+%Create a symbollic matrix
+symTheta = sym('Theta',[1, length(a)]);
+symTheta = transpose(symTheta);
+symD = sym('D',[1,length(a)]);
+symD = transpose(symD);
+%Create a symbollic DH matrix
+symA = sym('a',[1 length(a)]);
+symA = transpose(symA);
+symAlpha = sym('alpha', [1 length(a)]);
+symAlpha = transpose(symAlpha);
+
+for i = 1:length(a)
+    symAmat(:,:,i) = [cosd(symTheta(i)), -sind(symTheta(i))*cosd(symAlpha(i)), sind(symTheta(i))*sind(symAlpha(i)), symA(i)*cosd(symTheta(i));
+        sind(symTheta(i)), cosd(symTheta(i))*cosd(symAlpha(i)), -cosd(symTheta(i))*sind(symAlpha(i)), symA(1)*sind(symTheta(i));
+        0 sind(symAlpha(i)), cosd(symAlpha(i)), symD(i); 0 0 0 1];
+end
+
+
+
+
 %Create a starting matrix holding the first transformation matrix
 prev = A(:,:,1);
+prevsym = symAmat(:,:,1);
 
 for i = 1:length(a)
     
     if(i == 1)
         T(:,:,i) = prev;
+        symT(:,:,i) = prevsym;
     else 
         %Find the transformation matrix up to the current joint
         T(:,:,i) = prev*A(:,:,i);
+        symT(:,:,i) = prevsym*symAmat(:,:,i);
         %Update the holder for the previous transformation matrix
         prev = T(:,:,i);
+        prevsym = symT(:,:,i);
 
     end
 end
