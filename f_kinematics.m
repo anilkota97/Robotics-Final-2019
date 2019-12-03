161edc8bf3faa76246db8e938ea37e3911f8b2b8
anilkota97 <41119274+anilkota97@users.noreply.github.com>
2019-12-03 00:34:49 -0700

    forward kinematics updated

--- f_kinematics.m
+++ f_kinematics.m
@@ -3,14 +3,15 @@ function [pos_e, T] = f_kinematics(numJoints, jointTypes, linkLengths, zAxis, li
          
     % Substituting user input joint variables in DH parameters (confirm if numJoints includes end effector or not)
     k = symvar(symDH);
-    for i = 1:numJoints-1
+    [rows,cols] = size(symDH);
+    for i = 1:rows
         [row,col] = find(symDH == k(i));
         symDH(row,col) = q(i);
     end
     
     % A contains the transformation matrices between successive frames(confirm if alpha,theta are in radians in symDH)
     A = zeros(4,4,numJoints-1);
-    for i = 1:numJoints-1
+    for i = 1:rows
         A(:,:,i) = [cosd(symDH(i,4)), -sind(symDH(i,4))*cosd(symDH(i,3)), sind(symDH(i,4))*sind(symDH(i,3)), symDH(i,1)*cosd(symDH(i,4));
                     sind(symDH(i,4)), cosd(symDH(i,4))*cosd(symDH(i,3)), -cosd(symDH(i,4))*sind(symDH(i,3)), symDH(1,1)*sind(symDH(i,4));
                     0, sind(symDH(i,3)), cosd(symDH(i,3)), symDH(i,2); 
@@ -18,9 +19,9 @@ function [pos_e, T] = f_kinematics(numJoints, jointTypes, linkLengths, zAxis, li
     end
     
     % T contains the transformation matrices of frames w.r.t the base frame 
-    T = zeros(4,4,numJoints-1);
+    T = zeros(4,4,rows);
     prev = A(:,:,1);
-    for i = 1:numJoints-1
+    for i = 1:rows
         if i == 1
             T(:,:,i) = prev;
         else
